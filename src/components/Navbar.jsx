546d35e87014a24534e4b6e40237b2fca3f89918
SHIVA-6699 <pillashivakrishna6@gmail.com>
2024-09-18 11:52:18 +0530

    [logo]

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,23 +10,43 @@ const Navbar = () => {
   return (
     <nav className="bg-primary  px-6 py-5">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to={"/"} className="text-lg lg:text-2xl text-secondary font-bold font-heading duration-700">
-          Brick Wood Design Studio
-        </Link>
+          <Link
+            to={"/"}
+            className="text-lg flex items-center gap-x-3 lg:text-2xl text-secondary font-bold font-heading duration-700"
+          >
+            <img
+              src="/logo.png"
+              alt="IMGAE LOGO PNG"
+              className="w-10 h-10 bg-white rounded-full"
+            />
+            Brick Wood Design Studio
+          </Link>
 
         {/* Desktop Menu */}
         <div className="hidden md:flex space-x-10 text-xl">
-          <Link to="/" className="hover:text-black text-sm lg:text-lg text-secondary">
+          <Link
+            to="/"
+            className="hover:text-black text-sm lg:text-lg text-secondary"
+          >
             HOME
           </Link>
 
-          <Link to="/projects" className="hover:text-black text-sm lg:text-lg text-secondary">
+          <Link
+            to="/projects"
+            className="hover:text-black text-sm lg:text-lg text-secondary"
+          >
             PROJECTS
           </Link>
-          <Link to="/ourstory" className="hover:text-black text-sm lg:text-lg text-secondary">
+          <Link
+            to="/ourstory"
+            className="hover:text-black text-sm lg:text-lg text-secondary"
+          >
             OUR STORY
           </Link>
-          <Link to="/contact" className="hover:text-black text-sm lg:text-lg text-secondary">
+          <Link
+            to="/contact"
+            className="hover:text-black text-sm lg:text-lg text-secondary"
+          >
             CONTACT
           </Link>
         </div>
@@ -62,7 +82,10 @@ const Navbar = () => {
         <Link to="/" className="block py-2 px-4 text-white hover:bg-rise">
           Home
         </Link>
-        <Link to="/projects" className="block py-2 px-4 text-white hover:bg-rise">
+        <Link
+          to="/projects"
+          className="block py-2 px-4 text-white hover:bg-rise"
+        >
           Projects
         </Link>
         <Link
@@ -71,7 +94,10 @@ const Navbar = () => {
         >
           Our Story
         </Link>
-        <Link to="/contact" className="block py-2 px-4 text-white hover:bg-rise">
+        <Link
+          to="/contact"
+          className="block py-2 px-4 text-white hover:bg-rise"
+        >
           Contact
         </Link>
       </div>
